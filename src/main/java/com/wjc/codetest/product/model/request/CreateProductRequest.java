767892347d32b7d374a20b13217781e35101f5ba
j00hyeon <ljh221803@gmail.com>
2025-11-02 17:23:43 +0900

    refactor: 상품 생성 DTO 검증 추가 및 생성자 정리
    - @NotBlank 검증 추가
    - 불필요한 생성자 제거

--- src/main/java/com/wjc/codetest/product/model/request/CreateProductRequest.java
+++ src/main/java/com/wjc/codetest/product/model/request/CreateProductRequest.java
@@ -1,21 +1,27 @@
 package com.wjc.codetest.product.model.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * CreateProductRequest 개선
+ * [문제]
+ *  - 불필요한 생성자 다수 존재
+ *  - 검증 로직 부재
+ * [원인] JSON 직렬화 방식 이해 부족
+ * [개선안]
+ *  - 생성자 제거 (Jackson이 기본 생성자 + Setter 사용)
+ *  - 검증 로직 추가
+ */
 @Getter
 @Setter
+@NoArgsConstructor
 public class CreateProductRequest {
+    @NotBlank(message = "카테고리를 입력하세요.")
     private String category;
+
+    @NotBlank(message = "상품명을 입력하세요.")
     private String name;
-
-    public CreateProductRequest(String category) {
-        this.category = category;
-    }
-
-    public CreateProductRequest(String category, String name) {
-        this.category = category;
-        this.name = name;
-    }
 }
-
